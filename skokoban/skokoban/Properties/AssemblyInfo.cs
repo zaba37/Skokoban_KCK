46d2aeab31df147e788abfcd7206a3dfcb26a374
zaba <zaba37@gmail.com>
2015-09-28 18:06:14 +0200

    init

---
+++ skokoban/skokoban/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zbiór atrybutów. Zmień wartości tych atrybutów by zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("skokoban")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("skokoban")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie wartości ComVisible na false sprawia, że typy w tym zestawie nie będą widoczne 
+// dla składników COM.  Jeśli potrzebny jest dostęp do typu w tym zestawie z 
+// COM, ustaw wartość ComVisible na true, dla danego typu.
+[assembly: ComVisible(false)]
+
+// Następujący GUID jest dla ID typelib jeśli ten projekt jest dostępny dla COM
+[assembly: Guid("3e4469f4-b0d3-418d-8d7a-b5454f8c8139")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza 
+//      Numer kompilacji
+//      Rewizja
+//
+// Można określać wszystkie wartości lub używać domyślnych numerów kompilacji i poprawki 
+// poprzez użycie '*', jak pokazane jest poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
