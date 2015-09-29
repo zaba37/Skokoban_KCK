1a6bb924e48143de7979bca7673dc7c384020840
zaba <zaba37@gmail.com>
2015-09-29 16:10:02 +0200

    implement menu

--- skokoban/skokoban/Program.cs
+++ skokoban/skokoban/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace skokoban
+namespace sokoban
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
+            Console.SetWindowSize(170, 59);
+            //Console.SetWindowSize(170, 59);
+            Menu menu = new Menu();
+            Console.ReadKey();
         }
     }
 }
