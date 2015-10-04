bcb548a6235b4f23486f4fdc038a34c42aa4adde
piotrekb3 <pepeb333@gmail.com>
2015-10-04 19:48:21 +0200

    poprawione pkt

--- skokoban/skokoban/Game.cs
+++ skokoban/skokoban/Game.cs
@@ -773,6 +773,17 @@ namespace sokoban
                 }
             } while (true);
             timer.Stop();
+            DateTime ElapsedTime = DateTime.Parse(elapsedTime);
+            int totalSeconds = (ElapsedTime.Hour * 360) + (ElapsedTime.Minute * 60) + ElapsedTime.Second;
+            if (totalSeconds < 20)
+                totalPoints = totalPoints + 100;
+            if (totalSeconds >= 20 && totalSeconds <= 40)
+                totalPoints = totalPoints + 50;
+            if (totalSeconds > 40)
+                totalPoints = totalPoints + 20;
+            double pointsForSteps = ((double)numberSteps) * 0.1;
+
+            totalPoints = totalPoints - (int)pointsForSteps;
             EndGame endgame = new EndGame(totalPoints);
             endgame.run();
 
